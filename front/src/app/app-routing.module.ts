32f2b4211248537e10d9427bbca9257ac2d31988
Cuch0 <germanosimani@gmail.com>
2023-08-16 16:15:11 -0300

    login usuario

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   },
   {
     path: '**',
