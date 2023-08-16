34067b359755cba59afe323ec052718998bb7fa1
Irupe <lilianirupemeza@gmail.com>
2023-08-16 15:29:41 -0300

    feat: feature register

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
-    loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule),
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
   },
   {
     path: '**',
-    redirectTo: 'dashboard'
-  }
+    redirectTo: 'dashboard',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ front/src/app/register/register.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent {
+  nombres: string = '';
+  apellidos: string = '';
+  dni: string = '';
+  email: string = '';
+  celular: string = '';
+  personahumana: boolean = false;
+  empresa: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  onSubmit() {
+    if (this.password !== this.confirmPassword) {
+      console.log('Las contraseñas no coinciden');
+      return;
+    }
+
+    const nombreCompleto = `${this.nombres} ${this.apellidos}`;
+
+    const requestData = {
+      dni: this.dni,
+      personahumana: this.personahumana,
+      nombre: nombreCompleto,
+      email: this.email,
+      celular: this.celular,
+      empresa: this.empresa,
+      password: this.password,
+    };
+
+    console.log(requestData);
+
+    const URL = 'http://localhost:4001/clientes/';
+
+    this.http.post(URL, requestData).subscribe(
+      (response) => {
+        console.log('Registro exitoso', response);
+        this.router.navigate(['/dashboard']);
+      },
+      (error) => {
+        console.error('Error en el registro', error);
+      }
+    );
+  }
+}
